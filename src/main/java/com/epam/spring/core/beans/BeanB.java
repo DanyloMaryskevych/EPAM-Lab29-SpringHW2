730486e03826141863b3cf5f44791e0193a3ba2d
Danylo Maryskevych <danylo.maryskevych@gmail.com>
2021-07-14 18:18:47 +0300

    add Task1

---
+++ src/main/java/com/epam/spring/core/beans/BeanB.java
@@ -0,0 +1,32 @@
+package com.epam.spring.core.beans;
+
+public class BeanB {
+    private String name;
+    private int value;
+
+    public BeanB() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BeanB{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/main/java/com/epam/spring/core/beans/BeanE.java
@@ -0,0 +1,32 @@
+package com.epam.spring.core.beans;
+
+public class BeanE {
+    private String name;
+    private int value;
+
+    public BeanE() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "BeanE{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
