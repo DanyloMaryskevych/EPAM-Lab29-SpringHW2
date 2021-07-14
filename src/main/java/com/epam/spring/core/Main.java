3f2728907baf1be820f745d5d02e82ad56784bbe
Danylo Maryskevych <danylo.maryskevych@gmail.com>
2021-07-14 21:35:55 +0300

    add Task9

--- src/main/java/com/epam/spring/core/Main.java
+++ src/main/java/com/epam/spring/core/Main.java
@@ -10,8 +10,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("\t\t ***** Start Bean Initialization *****");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfigOne.class);
-        System.out.println("\t\t ***** End Bean Initialization *****");
-//        Arrays.asList(context.getBeanDefinitionNames()).forEach(System.out::println);
+        System.out.println("\t\t ***** End Bean Initialization *****\n");
+
+        Arrays.asList(context.getBeanDefinitionNames()).forEach(System.out::println);
 
         System.out.println("\n\t\t ***** Bean Destruction *****");
         context.close();
