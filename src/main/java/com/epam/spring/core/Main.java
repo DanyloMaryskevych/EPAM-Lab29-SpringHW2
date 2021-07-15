d8951b1e3a2583167fbfe7d0c04443545d4b6491
Danylo Maryskevych <danylo.maryskevych@gmail.com>
2021-07-15 17:41:10 +0300

    add Task12 and finish HW2

--- src/main/java/com/epam/spring/core/Main.java
+++ src/main/java/com/epam/spring/core/Main.java
@@ -3,6 +3,8 @@ package com.epam.spring.core;
 import com.epam.spring.core.config.AppConfigOne;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,7 +12,7 @@ public class Main {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfigOne.class);
         System.out.println("\t\t ***** End Bean Initialization *****\n");
 
-//        Arrays.asList(context.getBeanDefinitionNames()).forEach(System.out::println);
+        Arrays.asList(context.getBeanDefinitionNames()).forEach(System.out::println);
 
         System.out.println("\n\t\t ***** Bean Destruction *****");
         context.close();
